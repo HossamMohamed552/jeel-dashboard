4bb9f5c6084f6acdf10db3235f53048236ea11d0
Alaa ElHamouly <AElHamouly@sure.com.sa>
2023-05-13 16:48:07 +0300

    add user form

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,21 @@ export const getAllUsersRequest = () => ({
   url: 'users',
   config: {}
 })
+
+
+export const getRolesRequest = () => ({
+  method: 'get',
+  url: 'roles',
+  config: {}
+})
+
+export const postAddUserRequest = (data) => ({
+  method: "post",
+  url: "users",
+  config: {
+    data,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  },
+});
