b52ded6ed8ad32a0b6d2c574c02053a65d2ddaae
haythamfandaqa <125131669+haythamfandaqa@users.noreply.github.com>
2023-05-14 15:52:04 +0300

    Update index.vue

--- src/components/Modules/Users/AddEditUser/index.vue
+++ src/components/Modules/Users/AddEditUser/index.vue
@@ -153,20 +153,23 @@ export default {
         if (!success) return;
         this.handleUploadImage();
         const formData = new FormData();
-        // formData.append("first_name", this.formValues.first_name);
-        // formData.append("last_name", this.formValues.last_name);
-        // formData.append("email", this.formValues.email);
-        // formData.append("password", this.formValues.password);
-        // formData.append("mobile", this.formValues.mobile);
-        // formData.append("social_media", this.formValues.social_media);
-        // formData.append("roles", this.formValues.roles);
-        for (const key in this.formValues) {
-          formData.append(key, this.formValues[key]);
-        }
+        console.log(this.formValues.first_name)
+        formData.append("first_name",JSON.stringify(this.formValues.first_name));
+        formData.append("last_name",JSON.stringify(this.formValues.last_name));
+        formData.append("email",JSON.stringify(this.formValues.email));
+        formData.append("password",JSON.stringify(this.formValues.password));
+        formData.append("mobile",JSON.stringify(this.formValues.mobile));
+        formData.append("social_media",JSON.stringify(this.formValues.social_media));
+        formData.append("roles",JSON.stringify(this.formValues.roles));
+        // for (const key in this.formValues) {
+        //   // formData.append(key,JSON.stringify(this.formValues[key]);
+        //   console.log(this.formValues[key])
+        //   formData.append(key,JSON.stringify(this.formValues[key]))
+        // }
         // console.log(...formData.entries());
-        // console.log("formvalues", this.formValues);
+        // console.log("formvalues",JSON.stringify(this.formValues);
         // console.log("formdata", formData);
-        if (this.image) formData.append("image", this.image);
+        // if (this.image) formData.append("image",JSON.stringify(this.image);
         this.$emit("handleAddEditUser", formData);
       });
     },
