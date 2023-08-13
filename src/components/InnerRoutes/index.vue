a59ab04ca271f3e9b3199beaf07775b0d7decb09
Hossam Eldin Mohamed AbdelRazik <habdelrazik@sure.com.sa>
2023-08-13 11:58:53 +0300

    pull latest

--- src/components/InnerRoutes/index.vue
+++ src/components/InnerRoutes/index.vue
@@ -1,41 +1,48 @@
 <template>
   <section class="inner-routes">
     <div class="taps">
-      <div @click="activeTap = 1" :class="activeTap === 1 ? 'active' : ''" class="tap">
-        صلاحيات النظام والمستخدمين
+      <div @click="activeTapActive(1)"
+           :class="activeTap === 1 && Array.from(routesUsers).length>= 1 ? 'active' : ''"
+           class="tap" v-if="Array.from(routesUsers).length>= 1">صلاحيات النظام والمستخدمين
       </div>
-      <div @click="activeTap = 2" :class="activeTap === 2 ? 'active' : ''" class="tap">المدارس</div>
-      <div @click="activeTap = 3" :class="activeTap === 3 ? 'active' : ''" class="tap">المحتوي</div>
+      <div @click="activeTapActive(2)"
+           :class="activeTap === 2 && Array.from(routesSchool).length>= 1 ? 'active' : ''"
+           class="tap" v-if="Array.from(routesSchool).length>= 1">المدارس
+      </div>
+      <div @click="activeTapActive(3)"
+           :class="activeTap === 3 && Array.from(routesContent).length>= 1? 'active' : ''"
+           class="tap" v-if="Array.from(routesContent).length>= 1">المحتوي
+      </div>
     </div>
     <div class="content">
       <div class="row">
         <transition name="fade">
-          <div class="col-12 px-0" v-if="activeTap === 1">
+          <div class="col-12 px-0" v-if="activeTap === 1 && Array.from(routesUsers).length>= 1">
             <div class="row">
               <div class="col-lg-6 col-12" v-for="(item, index) in routesUsers" :key="index">
-                <RouteItem :item="item" />
+                <RouteItem :item="item"/>
               </div>
             </div>
           </div>
-          <div class="col-12 px-0" v-if="activeTap === 2">
+          <div class="col-12 px-0" v-if="activeTap === 2 && Array.from(routesSchool).length>= 1">
             <div class="row">
               <div
                 class="col-lg-3 col-sm-6 col-12"
                 v-for="(item, index) in routesSchool"
                 :key="index"
               >
-                <RouteItem :item="item" />
+                <RouteItem :item="item"/>
               </div>
             </div>
           </div>
-          <div class="col-12 px-0" v-if="activeTap === 3">
+          <div class="col-12 px-0" v-if="activeTap === 3 &&  Array.from(routesContent).length>= 1">
             <div class="row">
               <div
                 class="col-lg-3 col-sm-6 col-12"
                 v-for="(item, index) in routesContent"
                 :key="index"
               >
-                <RouteItem :item="item" />
+                <RouteItem :item="item"/>
               </div>
             </div>
           </div>
@@ -46,7 +53,7 @@
 </template>
 <script>
 import RouteItem from "@/components/RouteItem/index.vue";
-import { routesUsers, routesSchool, routesContent } from "@/globalData";
+import {routesUsers, routesSchool, routesContent} from "@/globalData";
 
 export default {
   name: "index",
@@ -65,6 +72,9 @@ export default {
     getRoutes(routeArr = []) {
       return routeArr.filter((route) => this.permissions?.includes(route.permission));
     },
+    activeTapActive(tap){
+      this.activeTap = tap
+    }
   },
   watch: {
     permissions: {
@@ -81,6 +91,15 @@ export default {
       return this.$store.getters.user?.permissions;
     },
   },
+  mounted() {
+    if (Array.from(this.routesUsers).length >= 1){
+      this.activeTapActive(1)
+    } else if(Array.from(this.routesSchool).length >= 1){
+      this.activeTapActive(2)
+    } else {
+      this.activeTapActive(3)
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
