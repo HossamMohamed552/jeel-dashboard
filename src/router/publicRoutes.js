974511581c044c4409f94965f9909c972d38973b
aelhamouly@sure.com.sa <aelhamouly@sure.com.sa>
2023-09-16 10:24:41 +0300

    initiate forget password page

--- src/router/publicRoutes.js
+++ src/router/publicRoutes.js
@@ -3,6 +3,11 @@ const publicRoutes = [
     path:"/login",
     name:"login",
     component: ()=> import("../../views/login/index.vue")
+  },
+  {
+    path:"/forget-password",
+    name:"forget-password",
+    component: ()=> import("../../views/forget-password/index.vue")
   }
 ];
 export const publicRoutesPaths = publicRoutes.map((route) => route.path);
