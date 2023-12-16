0e437ae82d03c72388ef87db3a698dfc1738ee0d
mahmoud amer <mamer@sure.com.sa>
2023-12-16 17:29:47 +0200

    fixes

---
+++ views/learning-skill/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <section class="container-fluid custom-container">
+    <ListItems
+      :header-name="'قائمة المهارات'"
+      :number-of-item="totalNumber"
+      :tableItems="LearningSkillsList"
+      :fieldsList="fieldsList"
+      :v-search-model="groupSearchWord"
+      @detailItem="detailItem($event)"
+      @editItem="editItem($event)"
+      @deleteItem="deleteItem($event)"
+      @refetch="getLearningSkills"
+      :loading="loading"
+      :permission_delete="'delete-terms'"
+      :permission_edit="'edit-terms'"
+      :permission_view="'show-terms'"
+    >
+      <template #buttons>
+        <Button
+          :custom-class="'btn-add rounded-btn big-padding'"
+          @click="goToAddLearningSkills"
+          v-if="user.permissions.includes(`add-terms`)"
+        >
+          <img src="@/assets/images/icons/plus.svg" />
+          <span>إضافة مهارة </span>
+        </Button>
+      </template>
+    </ListItems>
+    <Modal
+      :content-message="'حذف المهارة'"
+      :content-message-question="'هل انت متأكد من حذف المهارة '"
+      :showModal="showModal"
+      @cancel="cancel($event)"
+      :is-warning="true"
+      @cancelWithConfirm="cancelWithConfirm($event)"
+    />
+  </section>
+</template>
+
+<script>
+import Button from "@/components/Shared/Button/index.vue";
+import ListItems from "@/components/ListItems/index.vue";
+import { getLearningSkillsRequest, deleteLearningSkillRequest } from "@/api/learning-skill";
+import Modal from "@/components/Shared/Modal/index.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  components: { Modal, ListItems, Button },
+  data() {
+    return {
+      loading: false,
+      showModal: false,
+      groupSearchWord: "",
+      LearningSkillsList: [],
+      totalNumber: null,
+      fieldsList: [
+        { key: "id", label: "التسلسل" },
+        { key: "name", label: "اسم المهارة" },
+        { key: "actions", label: "الإجراء" },
+      ],
+    };
+  },
+  methods: {
+    goToAddLearningSkills() {
+      this.$router.push("/dashboard/learning-skill/add");
+    },
+    getLearningSkills(event) {
+      this.loading = true;
+      const params = event;
+      this.ApiService(getLearningSkillsRequest(params))
+        .then((response) => {
+          this.LearningSkillsList = response.data.data;
+          this.totalNumber = response.data.meta.total;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    detailItem($event) {
+      this.$router.push(`/dashboard/learning-skill/show/${$event}`);
+    },
+    editItem($event) {
+      this.$router.push(`/dashboard/learning-skill/edit/${$event}`);
+    },
+    deleteItem($event) {
+      this.itemId = $event;
+      this.showModal = true;
+    },
+    cancel($event) {
+      this.showModal = $event;
+    },
+    cancelWithConfirm() {
+      this.ApiService(deleteLearningSkillRequest(this.itemId)).then(() => {
+        this.getLearningSkills();
+      });
+      this.cancel();
+    },
+  },
+  computed: {
+    ...mapGetters(["user"]),
+  },
+  mounted() {
+    this.getLearningSkills();
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "./index";
+</style>
---
+++ views/learning-style/view-learning-style/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container-fluid custom-container">
+    <div class="show-term">
+      <div class="hold-fields">
+        <b-row> </b-row>
+        <b-row>
+          <b-col lg="6">
+            <ShowItem title="الاسم " :subtitle="LearningStyle.name" />
+          </b-col>
+        </b-row>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import ShowItem from "@/components/Shared/ShowItem/index.vue";
+import { getLearningStyleByIdRequest } from "@/api/learning-style";
+export default {
+  name: "index",
+  components: {
+    ShowItem,
+  },
+  data() {
+    return {
+      LearningStyle: {},
+    };
+  },
+  mounted() {
+    this.ApiService(getLearningStyleByIdRequest(this.$route.params.id)).then((response) => {
+      this.LearningStyle = response.data.data;
+    });
+  },
+};
+</script>
+<style scoped lang="scss">
+@import "./index";
+</style>
