6035c4e61d478534fc619a4d6f4bc169a8626212
Hossam Eldin Mohamed AbdelRazik <habdelrazik@sure.com.sa>
2024-02-12 18:49:50 +0200

    edit quiz

--- views/practices/add-practices/index.vue
+++ views/practices/add-practices/index.vue
@@ -35,10 +35,7 @@ export default {
         questions++
       }
 
-      for (let blooms = 0; blooms < quiz.blooms.length;) {
-        formData.append(`blooms[${blooms}]`, quiz.blooms[blooms]);
-        blooms++
-      }
+      formData.append(`blooms[0]`, quiz.blooms);
 
       for (let learning_styles = 0; learning_styles < quiz.learning_styles.length;) {
         formData.append(`learning_styles[${learning_styles}]`, quiz.learning_styles[learning_styles]);
