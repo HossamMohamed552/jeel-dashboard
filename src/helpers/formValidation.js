0c66d63ef29bec10abcf24ef77c7bacd3e3f00ed
keroloszakaria <44670424+keroloszakaria@users.noreply.github.com>
2024-04-15 15:11:10 +0200

    feat ( validation ): add compare validation

--- src/helpers/formValidation.js
+++ src/helpers/formValidation.js
@@ -1,4 +1,5 @@
-import {extend} from "vee-validate";
+import { extend } from "vee-validate";
+import moment from "moment";
 import {
   required,
   email,
@@ -7,7 +8,7 @@ import {
   size,
   max,
   max_value,
-  regex
+  regex,
 } from "vee-validate/dist/rules";
 
 extend("required", {
@@ -38,17 +39,28 @@ extend("max_value", {
   ...max_value,
 });
 
+extend("afterDate", {
+  validate(value, { compareValue }) {
+    const enteredDate = moment(value, "DD-MM-YYYY");
+    const comparisonDate = moment(compareValue, "DD-MM-YYYY");
+    if (moment(enteredDate).isAfter(comparisonDate)) {
+      return true;
+    }
+    return `يرجى إدخال بعد تاريخ ${compareValue}`;
+  },
+  params: ["compareValue"],
+});
 extend("size", {
   ...size,
   message: (_, value) => `File size must be below ${value.size / 1000} Mb`,
 });
-extend('audio', {
+extend("audio", {
   validate(value) {
-    const extension = value.name.split('.').pop().toLowerCase();
-    const audioExtensions = ['mp3', 'wav', 'ogg']
+    const extension = value.name.split(".").pop().toLowerCase();
+    const audioExtensions = ["mp3", "wav", "ogg"];
     if (audioExtensions.includes(extension)) {
-      return true
+      return true;
     }
-    return 'التسجيل الصوتى يجب ان يكون صوت';
-  }
-})
+    return "التسجيل الصوتى يجب ان يكون صوت";
+  },
+});
