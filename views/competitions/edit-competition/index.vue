9a963b0a32044d2894b0ce779e66016b6afb609a
Kerolos Zakaria <44670424+keroloszakaria@users.noreply.github.com>
2024-04-24 12:38:21 +0200

    fix ( competitions ): handle date format

--- views/competitions/edit-competition/index.vue
+++ views/competitions/edit-competition/index.vue
@@ -215,7 +215,7 @@ export default {
           type: "datetime",
           rules: "required",
           placeholder: "من",
-          format: "DD-MM-YYYY HH:mm",
+          format: "YYYY-MM-DD HH:mm",
         },
         {
           key: "end_date",
@@ -226,7 +226,7 @@ export default {
           type: "datetime",
           rules: "required",
           placeholder: "الى",
-          format: "DD-MM-YYYY HH:mm",
+          format: "YYYY-MM-DD HH:mm",
         },
         {
           key: "competition_time",
